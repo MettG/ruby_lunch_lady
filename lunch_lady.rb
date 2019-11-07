3c754a5d3b852afd3dbb052f06fb0887a74a8944
Tristan Johnson <tristan.t.johnson@gmail.com>
2019-11-07 13:28:24 -0700

    wallet complete

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -7,7 +7,21 @@ end
 class User
 	private
 		class Wallet
-
+			attr_accessor :amount
+			def initialize(dollars)
+				@amount = dollars
+			end
+			def deduct(val)
+				@amount -= val
+			end
+			def purchase(val)
+				if @amount >= val
+					deduct(val)
+					true
+				else
+					false
+				end
+			end
 		end
 end
 class Dish
