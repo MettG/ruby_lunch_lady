7fc8d33d3a041262443b95d5060ebc328db07f98
Tristan Johnson <tristan.t.johnson@gmail.com>
2019-11-07 12:45:46 -0700

    initial commit

---
+++ lunch_lady.rb
@@ -0,0 +1,27 @@
+require "pry"
+require "colorize"
+
+class Menu
+	puts
+end
+class User
+	private
+		class Wallet
+
+		end
+end
+class Dish
+
+end
+
+class MainDish < Dish
+
+end
+
+class SideDish < Dish
+
+end
+
+class LunchLady
+
+end
